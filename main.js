cf1425042b717f67af93e03dcbcd69c6503fdd56
vanshika308 <vanshikatiwari0308@gmail.com>
2023-06-07 22:17:19 +0530

    added delete functionality

--- main.js
+++ main.js
@@ -1,72 +1,71 @@
-  function submitUserDetails(event){
-    event.preventDefault();
-    var email = document.getElementById("email").value;
-    var name = document.getElementById("name").value;
-   // var storedObjects = JSON.parse(localStorage.getItem("storedObjects")) || [];
-    var userDetails = {
-        name: name,
-        email: email
-    };
-    //storedObjects.push(userDetails);
-  axios.post("https://crudcrud.com/api/874a145083894416a44eb349b611fc5c/appointmentData", userDetails)
-   .then((response)=>{
-    
-    console.log(response)
-  
-   })
-   .catch((error)=>{
-    console.log(error)
-   })
+function submitUserDetails(event) {
+  event.preventDefault();
+  var email = document.getElementById("email").value;
+  var name = document.getElementById("name").value;
+  var userDetails = {
+    name: name,
+    email: email
+  };
 
-    showUserOnScreen(userDetails);
- }
+  axios.post("https://crudcrud.com/api/663c8ab428a344cba11815b9dac23925/appointmentData", userDetails)
+    .then((response) => {
+      console.log(response.data);
+      showUserOnScreen(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-window.addEventListener("DOMContentLoaded",()=>{
-    axios.get("https://crudcrud.com/api/874a145083894416a44eb349b611fc5c/appointmentData")
-     .then((response)=>{
-       console.log(response)
-       for(var i=0;i<response.data.length;i++){
-        showUserOnScreen(response.data[i]);
-      }
-     })
-     .catch((error)=>{
-       console.log(error)
-     })
-})
+window.addEventListener("DOMContentLoaded", () => {
+  axios.get("https://crudcrud.com/api/663c8ab428a344cba11815b9dac23925/appointmentData")
+    .then((response) => {
+      console.log(response.data);
+      response.data.forEach((userDetails) => {
+        showUserOnScreen(userDetails);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-
- function showUserOnScreen(userDetails){
- 
+function showUserOnScreen(userDetails) {
   var userList = document.getElementById("users");
   const item = document.createElement('li');
-  item.setAttribute('class','element');
+  item.setAttribute('class', 'element');
 
-  const deleteButton =document.createElement('button');
-  deleteButton.setAttribute('class','btn-li');
-  deleteButton.textContent="delete";
-  const editButton =document.createElement('input');
-  editButton.type='button';
-  editButton.value="edit";
-  editButton.onclick=()=>{
-    localStorage.removeItem("userDetails");
-    parentItem.removeChild(item);
-    document.getElementById('name').value=userDetails.name;
-    document.getElementById('email').value=userDetails.email;
-  }
-  deleteButton.addEventListener("click",function(){
-    userList.removeChild(item);
-    localStorage.removeItem("userDetails");
+  const deleteButton = document.createElement('button');
+  deleteButton.setAttribute('class', 'btn-li');
+  deleteButton.textContent = "delete";
+  deleteButton.addEventListener("click", function () {
+    axios.delete("https://crudcrud.com/api/663c8ab428a344cba11815b9dac23925/appointmentData/" + userDetails._id)
+      .then((response) => {
+        console.log(response);
+        userList.removeChild(item);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   });
-  item.textContent=userDetails.name+" "+userDetails.email;
+
+  const editButton = document.createElement('input');
+  editButton.type = 'button';
+  editButton.value = "edit";
+  editButton.onclick = () => {
+    editUserDetails(userDetails, item);
+  };
+
+  item.textContent = userDetails.name + " " + userDetails.email;
   item.appendChild(deleteButton);
   item.appendChild(editButton);
-  const parentItem=document.getElementById('users');
-  parentItem.appendChild(item);
- }
- function editUserDetails(userDetails, listItem) {
-  var name = listItem.querySelector('.name').textContent;
-  var email = listItem.querySelector('.email').textContent;
-  
+  userList.appendChild(item);
+}
+
+function editUserDetails(userDetails, listItem) {
+  var name = userDetails.name;
+  var email = userDetails.email;
+
   // Populate the input fields with the extracted values
   document.getElementById('name').value = name;
   document.getElementById('email').value = email;
